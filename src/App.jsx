e1957e67abe8860cb31bd8a91298e38def7976ca
Deekshit7483 <deekshithhr3729@gmail.com>
2025-09-27 19:45:35 +0530

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Project</h1>
+        <p>
+          Welcome to your new React application!
+        </p>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
